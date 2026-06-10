d0bd82e81b7cf73eab72c305a44220c388cde2ee
Isa Clara <isaclarabispo@gmail.com>
2026-06-09 21:15:32 -0300

    Ajuste no settings.py/allowed_hosts para o deploy

--- cardapio/core/settings.py
+++ cardapio/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-elt1*g(gs!gv)!!k51lmks+-t9z7*77*n*427eq6q^8ja&x)-7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cardapio-python.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
